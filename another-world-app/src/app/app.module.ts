8090c23ca1912adaa2a5578759e7ed0f849cfe27
iam9 <ibrahim.asif.mirza@gmail.com>
2019-01-07 18:57:05 +0100

    new stuff

--- another-world-app/src/app/app.module.ts
+++ another-world-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes, Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,14 +10,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { materialModule } from './material';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
-import { RoleSelectComponent } from './role-select/role-select.component'
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTreeModule } from '@angular/material';
+import { RoleSelectComponent } from './role-select/role-select.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { BusinessComponent } from './business/business.component'
+import { AdminComponent } from './admin/admin.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+
+const appRoutes: Routes = [
+  { path: 'project-list', component: ProjectListComponent },
+  { path: 'business', component: BusinessComponent },
+  { path: 'admin', component: AdminComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    RoleSelectComponent
+    RoleSelectComponent,
+    ProjectListComponent,
+    AdminComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +47,14 @@ import { RoleSelectComponent } from './role-select/role-select.component'
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTreeModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    ),
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ another-world-app/src/app/project-list/project-list.component.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule, MatIconModule, MatTreeModule } from '@angular/material';
+
+import { ProjectListComponent } from './project-list.component';
+
+describe('ProjectListComponent', () => {
+  let component: ProjectListComponent;
+  let fixture: ComponentFixture<ProjectListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProjectListComponent ],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatTreeModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
